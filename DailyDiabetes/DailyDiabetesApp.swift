a8472d33331524bc418232d9fcc9cdbe70ce8d7f
vtodorchuk <bozodee8@gmail.com>
2024-09-08 14:00:16 +0300

    Initial Commit

---
+++ DailyDiabetes/DailyDiabetesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyDiabetesApp.swift
+//  DailyDiabetes
+//
+//  Created by Vlady Todorchuk on 08.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct DailyDiabetesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
